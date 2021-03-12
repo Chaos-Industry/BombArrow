41a753368e7e8d277de3da537ae0f9d4b3faad2d
hiroyuki-takayama-2021 <77880376+hiroyuki-takayama-2021@users.noreply.github.com>
2021-03-12 22:00:12 +0900

    Initial commit

---
+++ src/main/java/com/github/chaosindustory/bombarrow/BombArrow.java
@@ -0,0 +1,19 @@
+package com.github.chaosindustory.bombarrow;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class BombArrow extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        getServer().getPluginManager().registerEvents(new Arrow(), this);
+        Bukkit.getLogger().info("BarbedWire becomes valid!");
+
+    }
+
+    @Override
+    public void onDisable() {
+        Bukkit.getLogger().info("BarbedWire becomes invalid!");
+    }
+}
